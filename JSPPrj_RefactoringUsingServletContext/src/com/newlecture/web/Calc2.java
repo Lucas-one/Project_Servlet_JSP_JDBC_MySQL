41249f888ac3ca0fe3c20b465a5bc2d8ce714e91
Lucas-one <first1373@naver.com>
2020-01-20 15:53:53 +0900

    RefactoringUsingServletContext

---
+++ JSPPrj_RefactoringUsingServletContext/src/com/newlecture/web/Calc2.java
@@ -0,0 +1,51 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/calc2")
+public class Calc2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ServletContext application = request.getServletContext();
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String v_ = request.getParameter("v");
+		String op = request.getParameter("operator");
+		
+		int v = 0;
+		if(!v_.equals("")) v = Integer.parseInt(v_);
+		//°è»ê
+		if(op.equals("=")) {
+			
+			int x = (Integer)application.getAttribute("value");
+			int y = v;		
+			int result = 0;
+			
+			String operator = (String)application.getAttribute("op");
+			if(operator.equals("+")) result = x + y;
+			else if(operator.equals("-")) result = x - y;
+			
+			response.getWriter().printf("result is %d\n", result);
+		}
+			
+		
+		else {
+			
+			application.setAttribute("value", v);
+			application.setAttribute("op", op);
+		}
+	
+	}
+
+}
