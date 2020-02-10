a89a5e1f30ce5b8aca18e3cff78140bf341cff14
Lucas-one <first1373@naver.com>
2020-02-10 21:34:35 +0900

    Database_refactoring

--- JDBC/Mysql_DB_Table_Query.sql
+++ JDBC/Mysql_DB_Table_Query.sql
@@ -44,10 +44,13 @@ DELETE FROM notice WHERE id =1;
 DELETE FROM notice WHERE id =2;
 DELETE FROM notice WHERE id =3;
 
-insert into notice values(1, 'JDBC test','English','aaa',now(), 0,'');
-insert into notice values(2, 'JDBC test2','English2','bbb',now(), 10,'');
-insert into notice values(3, 'JDBC test3','English3','ccc',now(), 7,'');
-insert into notice values(4, 'JDBC test4','English3','ccc',now(), 13,'');
+insert into notice values(1, 'JDBC란 무엇인가?','nana','무엇일까?과연',now(), 3,'');
+insert into notice values(2, 'JDBC 드라이버 다운로드 방법','YJ','방법은 추후 업로드 예정',now(), 10,'');
+insert into notice values(3, '파라미터를 가지는 문장 실행하기','Dragon','이것도 추후',now(), 7,'');
+insert into notice values(4, 'JSP에서 JDBC사용하기 공부완료','YJ2','공부했어요',now(), 13,'');
+insert into notice values(5, 'Service 계층 추가하기','Kim','확인해보기',now(), 23,'');
+insert into notice values(6, '6번째 게시글','Lucas','으앙',now(), 3,'');
+insert into notice values(7, '더이상 쓸말이 없다.','Really','H',now(), 3,'');
 
 desc notice;
 select * from notice;
