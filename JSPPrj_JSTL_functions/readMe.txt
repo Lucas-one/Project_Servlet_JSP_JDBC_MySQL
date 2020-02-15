6b2fcf0c7206d32f32999328e80a21bf3fe6603b
Lucas-one <first1373@naver.com>
2020-02-15 18:34:40 +0900

    JSTL_functions

---
+++ JSPPrj_JSTL_functions/readMe.txt
@@ -0,0 +1,3 @@
+I use JSTL_functions.
+
+As a result, the format of the attachment(첨부파일) changed like output.png.
---
+++ JSPPrj_JSTL_functions/src/com/newlecture/web/Calculator.java
@@ -0,0 +1,39 @@
+package com.newlecture.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/calculator")
+public class Calculator extends HttpServlet{
+	
+	
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//getMethod할 때  GET,POST 대문자로 써야함에 주의!
+		if(request.getMethod().equals("GET")) {
+			System.out.println("GET 요청이 왔다.");
+		}
+		else if(request.getMethod().equals("POST")) {
+			System.out.println("POST 요청이 왔다.");
+		}
+		super.service(request, response);
+	}
+	
+	//기존에 있는거 Override한 것이기 때문에 앞에 쓸 필요가 없다.
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPOST 메소드가 호출되었습니다.");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet 메소드가 호출되었습니다.");
+	}
+	
+}
